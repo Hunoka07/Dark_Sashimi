9189f13db347340ef7e8818e9e6b913b3b3cb2e1
Hunoka07 <hct9282@gmail.com>
2025-06-30 11:06:57 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,28 +1,30 @@
 import threading
 
-VERSION = "2.0"
+VERSION = "X"
 CREATOR = "Created by Hunoka07"
-PROJECT_NAME = "Dark Sashimi v2"
+PROJECT_NAME = "Dark Sashimi"
 
 REQUIRED_PYTHON_VERSION = (3, 8)
 DEFAULT_THREADS = {
-    "Guerilla": 100,
+    "Guerilla": 80,
     "Saturation": 500,
-    "Annihilation": 3000
+    "Annihilation": 2500
 }
-PROXY_TIMEOUT = 7
+PROXY_TIMEOUT = 6
 
 PROXY_SOURCES = [
     "https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/http.txt",
-    "https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/http.txt",
-    "https://raw.githubusercontent.com/tarampampam/proxy-daily/master/proxy-list-raw.txt",
-    "https://raw.githubusercontent.com/clarketm/proxy-list/master/proxy-list-raw.txt",
     "https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies-http.txt",
-    "https://raw.githubusercontent.com/prxchk/proxy-list/main/http.txt"
+    "https://raw.githubusercontent.com/proxifly/free-proxy-list/main/proxies/protocols/http/data.txt",
+    "https://raw.githubusercontent.com/vakhov/fresh-proxy-list/master/http.txt",
+    "https://raw.githubusercontent.com/ErcinDedeoglu/proxies/main/proxies/http.txt",
+    "https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/http.txt",
+    "https://raw.githubusercontent.com/Zaeem20/FREE_PROXIES_LIST/master/http.txt",
+    "https://raw.githubusercontent.com/VolkanSah/Auto-Proxy-Fetcher/main/proxies.txt",
+    "https://raw.githubusercontent.com/clarketm/proxy-list/master/proxy-list-raw.txt",
 ]
-
-USER_AGENT_URL = "https://raw.githubusercontent.com/lorien/user-agent-list/master/user-agents.json"
-PROXY_VALIDATION_TARGET = "https://httpbin.org/get"
+USER_AGENT_URL = "https://raw.githubusercontent.com/brands/data/main/data/user-agents.json"
+PROXY_VALIDATION_TARGETS = ["https://httpbin.org/get", "https://api.ipify.org?format=json"]
 
 attack_stats = {
     "requests_sent": 0, "bytes_sent": 0, "http_ok": 0,
@@ -33,3 +35,4 @@ attack_stats = {
 stop_event = threading.Event()
 proxies = []
 user_agents = []
+
