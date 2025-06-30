8f47bcddd88a78b2aac3dc3404f8a4c4df55ef9a
Hunoka07 <hct9282@gmail.com>
2025-06-30 08:44:58 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,9 @@
-# -*- coding: utf-8 -*-
 import threading
 
-# --- Thông tin Dự án (Bạn có thể tùy chỉnh) ---
 VERSION = "1.0"
 CREATOR = "Created by Hunoka07"
 PROJECT_NAME = "Dark Sashimi"
 
-# --- Cấu hình Kỹ thuật ---
 REQUIRED_PYTHON_VERSION = (3, 8)
 DEFAULT_THREADS = {
     "Guerilla": 75,
@@ -15,7 +12,6 @@ DEFAULT_THREADS = {
 }
 PROXY_TIMEOUT = 6
 
-# --- Nguồn Tài nguyên (Cập nhật 2025) ---
 PROXY_SOURCES = [
     "https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/http.txt",
     "https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies-http.txt",
@@ -25,11 +21,11 @@ PROXY_SOURCES = [
     "https://raw.githubusercontent.com/saisuiu/Lionkings-Http-Proxys-Lists/main/free.txt",
     "https://raw.githubusercontent.com/Anonym0usWork1221/Free-Proxies/main/proxy_files/http_proxies.txt",
     "https://raw.githubusercontent.com/officialputuid/KangProxy/main/http.txt",
+    "https://raw.githubusercontent.com/HyperBeats/proxy-list/main/http.txt"
 ]
 USER_AGENT_URL = "https://raw.githubusercontent.com/elliotwutingfeng/latest-user-agents/main/latest-user-agents.json"
 PROXY_VALIDATION_TARGET = "https://httpbin.org/get"
 
-# --- Trạng thái Toàn cục (Không nên thay đổi) ---
 attack_stats = {
     "requests_sent": 0, "bytes_sent": 0, "http_ok": 0,
     "http_error": 0, "connect_error": 0, "timeout_error": 0,
